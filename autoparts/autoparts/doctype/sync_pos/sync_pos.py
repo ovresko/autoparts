fd0bce28fc56f35946974fbed5a1342b1d52971b
ovresko <32199620+ovresko@users.noreply.github.com>
2020-05-16 23:44:33 +0200

    Update sync_pos.py

--- autoparts/autoparts/doctype/sync_pos/sync_pos.py
+++ autoparts/autoparts/doctype/sync_pos/sync_pos.py
@@ -5,6 +5,40 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappeclient import FrappeClient
+
 
 class SyncPOS(Document):
 	pass
+
+def start_sync():
+	sp = frappe.get_single('Sync POS')
+	user = sp.user
+	pwd = sp.password
+	url = sp.server
+	do_sync = sp.sync
+	items = sp.sync_pos_item
+	if(user and url and pwd and do_sync and items):
+		conn = FrappeClient(url, user, pwd)
+		for dt in items:
+			if not dt.sync or not dt.document_type:
+				continue
+			result = []
+			_last = frappe.get_all(dt.document_type,fields=["name","modified"],order_by='modified asc',limit=1)
+			if _last:
+				li = _last[0]
+				result = conn.get_list(dt.document_type, fields = ['*'], filters = {'modified':(">", li.modified)})
+			elif dt.date_sync:
+				result = conn.get_list(dt.document_type, fields = ['*'], filters = {'modified':(">", dt.date_sync)})
+			if result:
+				#dt.date_sync = 
+				for val in result:
+					if frappe.db.exists(dt.document_type,val.name):
+						val.ignore_permissions = True
+						val.ignore_mandatory = True
+						val.save()
+					else:
+						val.ignore_permissions = True
+						val.ignore_mandatory = True
+						val.insert()
+				

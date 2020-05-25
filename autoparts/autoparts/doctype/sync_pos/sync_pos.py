bca7b7177d43b48bd0492f2d2b2482f90616823d
ovresko <32199620+ovresko@users.noreply.github.com>
2020-05-25 14:09:56 +0100

    Update sync_pos.py

--- autoparts/autoparts/doctype/sync_pos/sync_pos.py
+++ autoparts/autoparts/doctype/sync_pos/sync_pos.py
@@ -11,6 +11,21 @@ from autoparts.autoparts.doctype.sync_pos.frappeclient import FrappeClient
 class SyncPOS(Document):
 	pass
 
+@frappe.whitelist()
+def get_last_modified(doctype):
+	if doctype:
+		_last = frappe.get_all(doctype,fields=["name","modified"],order_by='modified desc',limit=1)
+		if _last:
+			li = _last[0]
+			if li:
+				lid =  li.modified.strftime("%Y-%m-%d %H:%M:%S.%f")
+				print("lid %s" % lid)
+				return lid
+		else:
+			# empty
+			return "empty"
+	return None
+
 def start_sync():
 	sp = frappe.get_single('Sync POS')
 	user = sp.user
@@ -22,21 +37,51 @@ def start_sync():
 		print("%s %s %s" % (url,user,pwd))
 		conn = FrappeClient(url, user, pwd)
 		for dt in items:
-			if not dt.sync or not dt.document_type:
+			if not dt.document_type:
 				continue
+				
+			# sync back
+			if dt.sync:
+				last_edit = conn.get_api(
+					"autoparts.autoparts.doctype.sync_pos.sync_pos.get_last_modified",
+							 params={"doctype":dt.document_type}
+				)
+				my_items = []
+				if last_edit and last_edit!= "empty":
+					my_items = frappe.db.get_list(dt.document_type, fields = ['*'], filters = {'modified':(">", last_edit),'docstatus':("<", 2)})
+				elif last_edit == "empty":
+					my_items = frappe.db.get_list(dt.document_type, fields = ['*'], filters = {'docstatus':("<", 2)})
+
+				if my_items:
+					for val in my_items:
+						if not val:
+							continue
+						val["doctype"] = dt.document_type
+
+						print("up val %s " % val)
+						val = frappe.get_doc(val)
+						if val:
+							val._original_modified = val.modified
+							val.flags.ignore_if_duplicate = True
+							val.flags.ignore_links = True
+							val.flags.ignore_permissions = True
+							val.flags.ignore_mandatory = True
+							val._bypass_modified = True
+							conn.update(val)
+
+
+
+			# sync up
 			result = []
-			_last = frappe.get_all(dt.document_type,fields=["name","modified"],order_by='modified desc',limit=1)
-			if _last:
-				li = _last[0]
-				if li:
-					lid =  li.modified.strftime("%Y-%m-%d %H:%M:%S.%f")
-					print("lid %s" % lid)
-					result = conn.get_list(dt.document_type, fields = ['*'], filters = {'modified':(">", lid),'docstatus':("<", 2)})
+			lid = get_last_modified(dt.document_type)
+			#_last = frappe.get_all(dt.document_type,fields=["name","modified"],order_by='modified desc',limit=1)
+			if lid and lid != "empty":
+				result = conn.get_list(dt.document_type, fields = ['*'], filters = {'modified':(">", lid),'docstatus':("<", 2)})
 			elif dt.date_sync:
 				dtd =  dt.date_sync.strftime("%Y-%m-%d %H:%M:%S.%f")
 				print("dt %s" % dtd)
 				result = conn.get_list(dt.document_type, fields = ['*'], filters = {'modified':(">", dtd),'docstatus':("<", 2)})
-			else:
+			elif lid == "empty":
 				result = conn.get_list(dt.document_type, fields = ['*'], filters = {'docstatus':("<", 2)})
 			if result:
 				#dt.date_sync = 
@@ -45,34 +90,34 @@ def start_sync():
 						continue
 					val["doctype"] = dt.document_type
 					
-					print("val %s " % val)
+					print("down val %s " % val)
 					val = frappe.get_doc(val)
 					
 					#print(val)
-					if frappe.db.exists(dt.document_type,val.name):
-						print("exists %s" % val.name)
-						val._original_modified = val.modified
-						val.flags.ignore_if_duplicate = True
-						val.flags.ignore_links = True
-						val.flags.ignore_permissions = True
-						val.flags.ignore_mandatory = True
-						val._bypass_modified = True
-						val.save(ignore_permissions=True, ignore_version=True)
-						frappe.db.commit()
-					else:
-						print("new %s" % val.name)
-						val._original_modified = val.modified
-						val.flags.ignore_if_duplicate = True
-						val.flags.ignore_links = True
-						val.flags.ignore_permissions = True
-						val.flags.ignore_mandatory = True
-						val.docstatus=None
-						val._bypass_modified = True
-						val.__islocal = True
-						val.insert(
-							ignore_permissions=True,
-							ignore_links=True, 
-							ignore_if_duplicate=True,
-							ignore_mandatory=True)
-						frappe.db.commit()
-				
+					#if frappe.db.exists(dt.document_type,val.name):
+					print("exists %s" % val.name)
+					val._original_modified = val.modified
+					val.flags.ignore_if_duplicate = True
+					val.flags.ignore_links = True
+					val.flags.ignore_permissions = True
+					val.flags.ignore_mandatory = True
+					val._bypass_modified = True
+					val.save(ignore_permissions=True, ignore_version=True)
+					frappe.db.commit()
+					#else:
+					#	print("new %s" % val.name)
+					#	val._original_modified = val.modified
+					#	val.flags.ignore_if_duplicate = True
+					#	val.flags.ignore_links = True
+					#	val.flags.ignore_permissions = True
+					#	val.flags.ignore_mandatory = True
+					#	val.docstatus=None
+					#	val._bypass_modified = True
+					#	val.__islocal = True
+					#	val.insert(
+					#		ignore_permissions=True,
+					#		ignore_links=True, 
+					#		ignore_if_duplicate=True,
+					#		ignore_mandatory=True)
+					#	frappe.db.commit()
+			

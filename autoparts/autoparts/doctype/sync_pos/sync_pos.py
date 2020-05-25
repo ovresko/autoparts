48bad59f30654e1fd6f05aa5c1558d48e781e86f
ovresko <32199620+ovresko@users.noreply.github.com>
2020-05-25 18:04:53 +0100

    Update sync_pos.py

--- autoparts/autoparts/doctype/sync_pos/sync_pos.py
+++ autoparts/autoparts/doctype/sync_pos/sync_pos.py
@@ -138,7 +138,7 @@ def start_sync():
 						try:
 							if not dt.date_sync or (getdate(val.modified) > getdate(dt.date_sync)):
 								dt.date_sync = getdate(val.modified)
-							print("exists %s" % val.name)
+							print("exists %s %s %s" % (val.modified,val.name,dt.date_sync)
 							val._original_modified = val.modified
 							val.flags.ignore_if_duplicate = True
 							val.flags.ignore_links = True

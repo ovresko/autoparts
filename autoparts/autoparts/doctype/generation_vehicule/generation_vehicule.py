e9ce1cee5f3bc866a986f7214e54188f56ca3f1a
ovresko <32199620+ovresko@users.noreply.github.com>
2020-06-02 00:51:19 +0100

    Update generation_vehicule.py

--- autoparts/autoparts/doctype/generation_vehicule/generation_vehicule.py
+++ autoparts/autoparts/doctype/generation_vehicule/generation_vehicule.py
@@ -11,6 +11,23 @@ class Generationvehicule(Document):
 	def validate(self):
 		marque = frappe.get_doc('Modele de vehicule',self.modele_vehicule)
 		self.nom_generation = marque.modele + ' '+self.generation
+		periode = ''		
+		if self.date_debut:
+			year,month,day = str(self.date_debut).split('-')
+			periode += month+'.'+year[-2:] +' - '
+			today = nowdate()
+			d = getdate(self.date_debut).year
+			self.age = getdate(today).year - d
+
+		if self.date_fin:
+			y,m,d = str(self.date_fin).split('-')
+			periode += m+'.'+y[-2:]
+		
+		#frappe.msgprint(periode)
+		if periode:
+			self.periode = '('+periode+')'
+		else:
+			self.periode = ''
 	def autoname(self):
 		if self.modele_vehicule:
                         marque = frappe.get_doc('Modele de vehicule',self.modele_vehicule)

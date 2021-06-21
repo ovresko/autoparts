4c1a415a57ee90b7c36b9373c565fff6e420e6ef
Mohammad Farid <zackseen@gmail.com>
2021-06-21 22:07:08 +0800

    changed tab to spaces

--- autoparts/autoparts/doctype/generation_vehicule/generation_vehicule.py
+++ autoparts/autoparts/doctype/generation_vehicule/generation_vehicule.py
@@ -10,32 +10,32 @@ from frappe.utils import nowdate, getdate
 
 
 class Generationvehicule(Document):
-	def validate(self):
-		marque = frappe.get_doc('Modele de vehicule',self.modele_vehicule)
-		self.nom_generation = marque.modele + ' '+self.generation
-		periode = ''		
-		if self.date_debut:
-			year,month,day = str(self.date_debut).split('-')
-			periode += month+'.'+year[-2:] +' - '
-			today = nowdate()
-			d = getdate(self.date_debut).year
-			self.age = getdate(today).year - d
+    def validate(self):
+        marque = frappe.get_doc('Modele de vehicule',self.modele_vehicule)
+        self.nom_generation = marque.modele + ' '+self.generation
+        periode = ''        
+        if self.date_debut:
+            year,month,day = str(self.date_debut).split('-')
+            periode += month+'.'+year[-2:] +' - '
+            today = nowdate()
+            d = getdate(self.date_debut).year
+            self.age = getdate(today).year - d
 
-		if self.date_fin:
-			y,m,d = str(self.date_fin).split('-')
-			periode += m+'.'+y[-2:]
-		
-		#frappe.msgprint(periode)
-		if periode:
-			self.periode = '('+periode+')'
-		else:
-			self.periode = ''
-	def autoname(self):
-		if self.modele_vehicule:
+        if self.date_fin:
+            y,m,d = str(self.date_fin).split('-')
+            periode += m+'.'+y[-2:]
+        
+        #frappe.msgprint(periode)
+        if periode:
+            self.periode = '('+periode+')'
+        else:
+            self.periode = ''
+    def autoname(self):
+        if self.modele_vehicule:
             marque = frappe.get_doc('Modele de vehicule',self.modele_vehicule)
             code = marque.code_interne
-			self.nom_generation = marque.modele + ' '+self.generation
+            self.nom_generation = marque.modele + ' '+self.generation
             self.code_interne = make_autoname(code + '.##')
-		if not self.code_interne:
-			self.code_interne = self.generation
-		#self.name = self.code_interne
+        if not self.code_interne:
+            self.code_interne = self.generation
+        #self.name = self.code_interne
